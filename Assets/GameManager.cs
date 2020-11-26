837d20a9262675ac322afff151ebfa26c4dc6682
sahar <sahar.malter1@gmail.com>
2020-11-26 15:36:58 +0200

    upload of the project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static int score;
+    public TextMeshProUGUI score_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score_text.text = score.ToString();
+        if (score == 6)
+        {
+            Debug.Log("enough points for next level");
+            next_level();
+        }
+
+    }
+
+    public void next_level()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/cake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class cake : MonoBehaviour
+{
+
+    public GameObject Cake_pf;
+    public Transform spown_loacation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+     GetComponent<Collider>().enabled = false;
+        StartCoroutine(cake_it());
+    }
+
+    IEnumerator cake_it()
+    {
+        Debug.Log("yum yum, i ate the cake");
+        Debug.Log("points +1");
+     GameManager.score++;
+     yield return new WaitForSeconds(0.2f);
+     GetComponent<Collider>().enabled = true;
+     GameObject new_cake = Instantiate(Cake_pf, spown_loacation.position, spown_loacation.rotation);
+     new_cake.transform.localScale /= (1.5f+(GameManager.score/5));
+        Debug.Log("OOHHH a new cake !!");
+
+        Destroy(gameObject);
+    }
+
+}
